c4d3b31c6159d4e161823ae42c0fc919ebfd3228
mm-zb <zayanbaig01@gmail.com>
2025-08-21 00:25:19 +0100

    Include testing functionality for object identification

--- python/image_detection.py
+++ python/image_detection.py
@@ -4,7 +4,7 @@ from text_to_speech import TTSManager
 model = YOLO('yolov8n.pt')
 tts = TTSManager()
 
-def detect_items(image_path="test_images/fishing.jpeg"):
+def detect_items(image_path="test_images/fishing.jpeg", save=False):
 
     #image_path = 'test_images/pizza.jpeg' # can either have pi store image in folder or pass it directly
 
@@ -16,6 +16,8 @@ def detect_items(image_path="test_images/fishing.jpeg"):
             print(f"Object: {object_name}, Confidence: {confidence:.2f}")
             tts.queue_object(object_name, confidence)
     
-    tts.save_queue_to_wav()
-    #tts.speak_queue()
+    if save:
+        tts.save_queue_to_wav()
+    else:
+        tts.speak_queue()
 
